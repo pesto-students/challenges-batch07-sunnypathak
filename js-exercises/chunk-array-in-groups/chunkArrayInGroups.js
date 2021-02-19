f5f7594354943096b806b4fe9536624c2e0f2671
unknown <sp140898@gmail.com>
2021-02-19 20:56:49 +0530

    Change Splice logic to slice

--- js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
@@ -2,7 +2,7 @@ function chunkArrayInGroups(array, chunkSize) {
   if (chunkSize === 0) return array;
   return new Array(Math.ceil(array.length / chunkSize))
     .fill()
-    .map(() => array.splice(0, chunkSize));
+    .map((el, index) => array.slice(index * chunkSize, (index * chunkSize) + chunkSize));
 }
 
 export {
